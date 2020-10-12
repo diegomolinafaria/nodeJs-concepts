2388d1b7ec791d1c9e8d4e1c0876087251b45794
Diego Molina <dm@diegomolina.com.br>
2020-10-12 19:57:00 +0100

    Create crud with nodeJS concepts

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,59 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs, like } = request.body;
+  const repository = { id: uuid(), title, url, techs, likes: 0};
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repoIndex = repositories.findIndex(r => r.id === id);
+
+  if (repoIndex < 0) {
+      return response.status(400).json({error: "Repository not found"});
+  }
+  const likeCount = repositories[repoIndex].likes
+  const repository = { id, title, url, techs, likes : likeCount };
+
+  repositories[repoIndex] = repository;
+  return response.status(200).json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex(r => r.id === id);
+
+  if (repoIndex < 0) {
+      return response.status(400).json({error: "Repository not found"});
+  }
+
+  repositories.splice(repoIndex, 1);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  
+  const repoIndex = repositories.findIndex(r => r.id === id);
+
+  if(repoIndex < 0){
+    return response.status(400).json({error: 'Id not found.'});
+  }
+
+  const repository = repositories[repoIndex];
+  const incrementLike = 1;
+  repository.likes += incrementLike;
+  return response.status(201).json(repository);
 });
 
 module.exports = app;
